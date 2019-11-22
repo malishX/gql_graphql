9b009843615760b861e5c1d2f49417faf7287ea3
Omar Al-Mashjari <omar.almashjari@schoolvoice.com>
2019-11-22 23:12:06 +0400

    refactored Contact type resolvers

--- schema/Contact.js
+++ schema/Contact.js
@@ -1,28 +1,8 @@
-const graphql = require('graphql');
-const {MessageType, MessageTypeObj} = require('./Message');
-const {FlagType, FlagTypeObj} = require('./Flag');
-const {SchoolType, SchoolTypeObj} = require('./School'); 
-const {StudentType, StudentTypeObj} = require('./Student');
+const {MessageTypeObj} = require('./Message');
+const {FlagTypeObj} = require('./Flag');
+const {SchoolTypeObj} = require('./School'); 
+const {StudentTypeObj} = require('./Student');
 const db = require('../db');
-
-const {
-    GraphQLObjectType,
-    GraphQLID,
-    GraphQLString,
-    GraphQLList,
-    GraphQLInt,
-    GraphQLBoolean,
-    GraphQLEnumType
-} = graphql;
-
-const contactTypeEnum = new GraphQLEnumType({
-    name: "ContactType",
-    values: {
-        guardian: {value: "guardian"},
-        staff: {value: "staff"},
-        student: {value: "student"}
-    }
-});
 
 const ContactTypeObj = (response) => {
     return {
@@ -49,165 +29,124 @@ const ContactTypeObj = (response) => {
     }
 }
 
-const ContactType = new GraphQLObjectType({
-    name: 'Contact',
-    fields: ()=>({
-        id: {type: GraphQLID},
-        name: {type: GraphQLString},
-        mobile: {type: GraphQLString},
-        email: {type: GraphQLString},
-        image: {type: GraphQLString},
-        contact_type_id: {type: GraphQLString},
-        type: {
-            type: FlagType,
-            resolve: (parent)=>{
-                let query = "SELECT id, name FROM contact_type WHERE id="+parent.contact_type_id;
-                console.log(query);
-                let result = db.get(query).then(function(response){
-                    return FlagTypeObj(response[0].id, response[0].name)
-                }).catch(function(err){
-                    console.log(err);
-                });
-                return result;
-            }
-        },
-        device_type: {type: GraphQLString},
-        device_id: {type: GraphQLString},
-        voip_device_id: {type: GraphQLString},
-        build_version: {type: GraphQLString},
-        os_version: {type: GraphQLString},
-        app_version: {type: GraphQLString},
-        model: {type: GraphQLString},
-        device_pin: {type: GraphQLString},
-        latitude: {type: GraphQLString},
-        longitude: {type: GraphQLString},
-        street_name: {type: GraphQLString},
-        created_time: {type: GraphQLString},
-        created_by: {type: GraphQLString},
-        status: {type: GraphQLString},
-        messages: {
-            type: new GraphQLList(MessageType),
-            args: {
-                // returns latest N messages
-                first: {type: GraphQLInt},
-                as: {type: contactTypeEnum}
-            },
-            resolve(parent, args, context){
+const Contact = {
+    type(parent){
+        let query = "SELECT id, name FROM contact_type WHERE id="+parent.contact_type_id;
+        console.log(query);
+        let result = db.get(query).then(function(response){
+            return FlagTypeObj(response[0].id, response[0].name)
+        }).catch(function(err){
+            console.log(err);
+        });
+        return result;
+    },
 
-                // Store the contact_id in the context to use it in the messages' kids' resovler
-                //TODO make sure this is relevant to the current request only
-                context.contact_id = parent.id;
+    messages(parent, {first, as}, context) {
+        // Store the contact_id in the context to use it in the messages' kids' resovler
+        //TODO make sure this is relevant to the current request only
+        context.contact_id = parent.id;
 
-                // return all messages mapped to this parent.id (contact.id)
-                // That have been 1. Approved or doesn't need approval 2. Not draft 3. Not SMS message
-                // Ordered from newest to oldest
-                let query = `SELECT 
-                messages.id, 
-                messages.created,
-                messages.message, 
-                messages.amount, 
-                messages.action_type_id, 
-                messages.message_type_id, 
-                messages.sender_type_id,
-                messages.created_by, 
-                messages.school_id, 
-                messages.is_scheduled, 
-                messages.scheduled_time
-                FROM messages_mapping INNER JOIN messages ON messages_mapping.message_id=messages.id 
-                WHERE messages_mapping.contact_id= `+parent.id+`
-                AND messages.approval_status in (0,2)
-                AND messages.message_type_id != '4' 
-                AND messages.is_draft = 'no' `;
-                
-                if (args.as == 'guardian') // 'All Feeds' tab
-                    query += ` AND messages_mapping.guardian_id != 0 `
-                if (args.as == 'staff') // 'My Messages' tab
-                    query += ` AND messages_mapping.staff_id != 0 `
-                
-                // TODO handle messages as student
+        // return all messages mapped to this parent.id (contact.id)
+        // That have been 1. Approved or doesn't need approval 2. Not draft 3. Not SMS message
+        // Ordered from newest to oldest
+        let query = `SELECT 
+        messages.id, 
+        messages.created,
+        messages.message, 
+        messages.amount, 
+        messages.action_type_id, 
+        messages.message_type_id, 
+        messages.sender_type_id,
+        messages.created_by, 
+        messages.school_id, 
+        messages.is_scheduled, 
+        messages.scheduled_time
+        FROM messages_mapping INNER JOIN messages ON messages_mapping.message_id=messages.id 
+        WHERE messages_mapping.contact_id= `+parent.id+`
+        AND messages.approval_status in (0,2)
+        AND messages.message_type_id != '4' 
+        AND messages.is_draft = 'no' `;
+        
+        if (as == 'guardian') // 'All Feeds' tab
+            query += ` AND messages_mapping.guardian_id != 0 `
+        if (as == 'staff') // 'My Messages' tab
+            query += ` AND messages_mapping.staff_id != 0 `
+        
+        // TODO handle messages as student
 
-                query += ` ORDER BY messages_mapping.created DESC `;
+        query += ` ORDER BY messages_mapping.created DESC `;
 
-                if (args.first != null)
-                query += ` LIMIT ` + args.first;
+        if (first != null)
+        query += ` LIMIT ` + first;
 
-                return result = db.get(query).then(function(response){
-                    return response.map((message)=>{
-                        return MessageTypeObj(message);
-                    });
-                }).catch(function(err){
-                    console.log(err);
-                });
-            }
-        },
-        children: {
-            type: GraphQLList(StudentType),
-            resolve: parent => {
-                let query = `
-                SELECT
-                    * 
-                FROM
-                    students 
-                WHERE
-                    id IN (
-                    SELECT
-                        guardian_student_mapping.student_id 
-                    FROM
-                        guardian
-                        INNER JOIN guardian_student_mapping ON guardian.id = guardian_student_mapping.guardian_id 
-                WHERE
-                    contact_id = ` + parent.id + `)`;
-                return db.get(query).then( response => {
-                    return response.map(kid => {
-                        return StudentTypeObj(kid);
-                    });
-                }).catch( err => console.log(err));
-            }
-        },
-        schools: {
-            // returns a list of schools a contact is enrolled in
-            type: GraphQLList(SchoolType),
-            args: {
-                as: {type: contactTypeEnum}
-            },
-            resolve: (parent, args) => {
-                let query;
-                if (args.as == "guardian")
-                    query = `SELECT schools.* FROM contacts
-                    JOIN guardian ON contacts.id = guardian.contact_id
-                    JOIN schools ON schools.id = guardian.school_id
-                    WHERE 
-                        contacts.id = ` + parent.id;
-                else if (args.as == "staff")
-                    query = `SELECT schools.* FROM contacts
-                    JOIN staffs ON contacts.id = staffs.contact_id
-                    JOIN schools ON schools.id = staffs.school_id
-                    WHERE 
-                        contacts.id = ` + parent.id;
-                else // Load all messages sent to this contact regardless of his type
-                    query = `
-                    SELECT DISTINCT
-                        schools.* 
-                    FROM
-                        contacts
-                        JOIN guardian ON contacts.id = guardian.contact_id
-                        JOIN staffs ON guardian.contact_id = staffs.contact_id
-                        JOIN schools ON schools.id = staffs.school_id 
-                        OR schools.id = guardian.school_id 
-                    WHERE
-                        contacts.id = ` + parent.id;
-                // TODO handle students schools
-                return db.get(query).then( response => {
-                    return response.map(school => {
-                        return SchoolTypeObj(school);
-                    })
-                }).catch(err => console.log(err));
-            }
-        }
-    })
-});
+        return result = db.get(query).then(function(response){
+            return response.map((message)=>{
+                return MessageTypeObj(message);
+            });
+        }).catch(function(err){
+            console.log(err);
+        });
+    },
+
+    children(parent) {
+        let query = `
+        SELECT
+            * 
+        FROM
+            students 
+        WHERE
+            id IN (
+            SELECT
+                guardian_student_mapping.student_id 
+            FROM
+                guardian
+                INNER JOIN guardian_student_mapping ON guardian.id = guardian_student_mapping.guardian_id 
+        WHERE
+            contact_id = ` + parent.id + `)`;
+        return db.get(query).then( response => {
+            return response.map(kid => {
+                return StudentTypeObj(kid);
+            });
+        }).catch( err => console.log(err));
+    },
+
+    schools(parent, {as}) {
+        // returns a list of schools a contact is enrolled in
+        let query;
+        if (as == "guardian")
+            query = `SELECT schools.* FROM contacts
+            JOIN guardian ON contacts.id = guardian.contact_id
+            JOIN schools ON schools.id = guardian.school_id
+            WHERE 
+                contacts.id = ` + parent.id;
+        else if (as == "staff")
+            query = `SELECT schools.* FROM contacts
+            JOIN staffs ON contacts.id = staffs.contact_id
+            JOIN schools ON schools.id = staffs.school_id
+            WHERE 
+                contacts.id = ` + parent.id;
+        else // Load all messages sent to this contact regardless of his type
+            query = `
+            SELECT DISTINCT
+                schools.* 
+            FROM
+                contacts
+                JOIN guardian ON contacts.id = guardian.contact_id
+                JOIN staffs ON guardian.contact_id = staffs.contact_id
+                JOIN schools ON schools.id = staffs.school_id 
+                OR schools.id = guardian.school_id 
+            WHERE
+                contacts.id = ` + parent.id;
+        // TODO handle students schools
+        return db.get(query).then( response => {
+            return response.map(school => {
+                return SchoolTypeObj(school);
+            })
+        }).catch(err => console.log(err));
+    }
+};
 
 module.exports = {
-    ContactType,
+    Contact,
     ContactTypeObj
 }
--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,7 +1,8 @@
 const { merge } = require('lodash');
 const RootQuery = require('./RootQuery');
 const {Message} = require('./Message');
+const {Contact} = require('./Contact');
 
-const resolvers = merge(RootQuery, {Message});
+const resolvers = merge(RootQuery, {Message}, {Contact});
 
 module.exports = resolvers;
