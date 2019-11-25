c969104429b5b78a802808cbd6792777138da479
Omar Al-Mashjari <omar.almashjari@schoolvoice.com>
2019-11-25 11:54:28 +0400

    added contact_id arg to schema

--- schema/schema.js
+++ schema/schema.js
@@ -91,7 +91,7 @@ type Message {
 type Mutation {
     setMessageAction(message_id: ID!, contact_id: ID!, action_status: ActionStatus!): String
     updateProfile(contact_id: ID!, name: String, email: String): String
-    updateProfileImage(file: Upload!): String!
+    updateProfileImage(contact_id: ID!, file: Upload!): String!
 }
   
 type RootQuery {
